c045f5d614ecc53cc5c3394e2f34d617244feea3
esantb <e.saint.bou@gmail.com>
2023-02-23 18:02:14 -0600

    working on game function

--- blackjack.py
+++ blackjack.py
@@ -15,54 +15,93 @@ class Card:
 
 
 class Deck:
+
     def __init__(self):
         self.cards = []
         self.add_cards()
+
+    def __str__(self):
+        output = []
+        for card in self.cards:
+            output.append(str(card))
+        return str(output)
 
     def add_cards(self):
         for suit in SUITS:
             for rank in RANKS:
                 new_card = Card(suit, rank)
+                # print(new_card)
                 self.cards.append(Card(suit, rank))
 
     def shuffle(self):
-        random.shuffle(self.deck)
+        random.shuffle(self.cards)
+
+    def draw_cards(self, n):
+        drawn_cards = []
+        for i in range(n):
+            drawn_cards.append(self.cards.pop(0))
+        return drawn_cards
 
 
 class Player:
     def __init__(self):
-        self.name = input("What is your name? ")
+        # self.name = input("What is your name? ")
         self.hand = []
+        self.score = 0
+        # TODO CHANGE BELOW THIS LINE
+        self.name = "emmaline"
 
     def __str__(self):
         return f'{self.name} is the player'
 
-    def view_cards(self):
+# setting parameter (communicates between two separate classes) of card to connect the two objects
+    def add_cards_to_player(self, cards):
+        self.hand.extend(cards)
+
+    def view_hand(self):
         for card in self.hand:
             print(card)
 
     def calculate(self):
-        card_value = 0
-        score = 0
-        for card in self.hand:
-            if card.rank in ['J', 'Q', 'K']:
-                card_value = 10
-            elif card.rank == 'A':
-                if score >= 11:
-                    card_value = 1
-                else:
-                    card_value = 11
-            else:
-                card_value = card.rank
-            score += card_value
-        print('Player score: ', score)
-        return score
+        pass
+        # card_value = 0
+        # score = 0
+        # for card in self.hand:
+        #     if card.rank in ['J', 'Q', 'K']:
+        #         card_value = 10
+        #     elif card.rank == 'A':
+        #         if score >= 11:
+        #             card_value = 1
+        #         else:
+        #             card_value = 11
+        #     else:
+        #         card_value = card.rank
+        #     score += card_value
+        # print('Player score: ', score)
+        # return score
+
+        # hand_value = 0
+        # aces = 0
+        # for card in self.hand:
+        #     if card.rank == 'A':
+        #         aces += 1
+        #     elif card.rank in ['K', 'Q', 'J']:
+        #         hand_value += 10
+        #     else:
+        #         hand_value += card.rank
+        # for i in range(aces):
+        #     if hand_value + 11 > 21:
+        #         hand_value += 1
+        #     else:
+        #         hand_value += 11
+        # return hand_value
 
 
 class Dealer(Player):
     def __init__(self):
         self.name = 'Dealer'
         self.hand = []
+        self.score = 0
 
     def __str__(self):
         # when we write cariables and methods with the same
@@ -71,71 +110,72 @@ class Dealer(Player):
         return f'{self.name} is the dealer'
 
     def calculate(self):
-        card_value = 0
-        score = 0
-        for card in self.hand:
-            if card.rank in ['J', 'Q', 'K']:
-                card_value = 10
-            elif card.rank == 'A':
-                if score >= 11:
-                    card_value = 1
-                else:
-                    card_value = 11
-            else:
-                card_value = card.rank
-            score += card_value
-        print('Dealer score: ', score)
-        return score
+        pass
+        # card_value = 0
+        # score = 0
+        # for card in self.hand:
+        #     if card.rank in ['J', 'Q', 'K']:
+        #         card_value = 10
+        #     elif card.rank == 'A':
+        #         if score >= 11:
+        #             card_value = 1
+        #         else:
+        #             card_value = 11
+        #     else:
+        #         card_value = card.rank
+        #     score += card_value
+        # print('Dealer score: ', score)
+        # return score
 
 
 class Game:
     def __init__(self):
         self.player = Player()
         self.dealer = Dealer()
-        self.setup()
-
-    def setup(self):
-        # creates a deck
         self.deck = Deck()
-        # adds card for deck
-        self.deck.add_cards()
-        # for card in self.deck.cards:
-        #     print(card)
+        self.deck.shuffle()
+        self.deal()
+
+    def play_game(self):
+        print("Welcome to Blackjack!")
 
     def deal(self):
-        self.setup()
-        self.deck.shuffle()
-        print(new_game.player)
-        card = self.deck.cards.pop()
-        self.player.hand.append(card)
-        card = self.deck.cards.pop()
-        self.player.hand.append(card)
-        print(f'{self.player.name}s hand is ')
-        self.player.view_cards()
-        print(new_game.dealer)
-        card = self.deck.cards.pop()
-        self.dealer.hand.append(card)
-        card = self.deck.cards.pop()
-        self.dealer.hand.append(card)
-        print('Dealer hand is ')
-        self.dealer.view_cards()
+        dealing_it = self.deck.draw_cards(2)
+        self.player.add_cards_to_player(dealing_it)
 
-    def player_turn(self):  # player decides how many times to hit before playing
+    def player_turn(self, stay):  # player decides how many times to hit before playing
         pass
 
     def dealer_turn(self):
         pass
 
+# sets game-ending parameters
+    def end_game(self, winner=None):
+        pass
 
-new_game = Game()
 
-# TO DO
-# make a player, like we did with Deck
-# make a dealer, also like we did with Deck
-# play game
-# shuffle deck
-# deal cards
-# players turn first (hit/stay)
-# calculate score from cards in hand
-# who won/lost/busted/21
-# dealers turn
+if __name__ == '__main__':
+
+    '''
+    # initialize the deck function
+    my_deck = Deck()
+    # calling the deck class, then specifying the shuffle method
+    my_deck.shuffle()
+    print(my_deck)
+    # initializing the player
+    player1 = Player()
+    print(player1)
+    # taking juice from deck object, removing it from deck object
+    x = my_deck.draw_cards(2)
+    print(x)
+    # putting the juice into the players hand
+    # x as parameter: communicates between classes
+    player1.add_cards_to_player(x)
+    player1.view_hand()
+
+    # print(my_deck.cards[0])
+    # my_deck.draw_cards(1)
+    # print(my_deck.draw_cards(1)[0])
+    # print(drawn_cards)
+    # calculate()
+'''
